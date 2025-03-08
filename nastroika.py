7a99532686e30fab141841a44d707c215d7aadf8
mac <burbor@mail.ru>
2025-03-08 17:31:02 +0300

    Add main with hello

---
+++ nastroika.py
@@ -0,0 +1,12 @@
+def sum_factorial(num):
+    factorial = 1
+    total = 0
+    for i in range(1, num):
+        factorial *= i
+        total += factorial
+    return total
+
+
+number = int(input("Number: "))
+fac_sum = sum_factorial(number)
+print("fac sum for", number, "is:", fac_sum)
