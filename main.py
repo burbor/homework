0910359ca263c0d833943069d0b5abdadcebad03
mac <burbor@mail.ru>
2025-03-26 10:18:44 +0300

    hw_2

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print('Hello World!')
+def summ(x):
+    sum_number = 0
+    while x >= 1:
+        sum_number += x % 10
+        x //= 10
+    return sum_number
+
+
+def num(x):
+    num_number = 0
+    while x > 1:
+        x /= 10
+        num_number += 1
+    return num_number
+
+
+number = int(input('Введите число:'))
+print('Сумма чисел:', summ(number))
+print('Количество цифр:', num(number))
+difference = summ(number) - num(number)
+print('Разность суммы и количества цифр:', difference)
